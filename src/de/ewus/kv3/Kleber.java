cb50fbad53f8493ef79412519c7830f13fe66f90
e-man <e-man@96306714-77e5-0310-ad66-ff27f11abc03>
2004-10-16 20:28:38 +0000

    Methode zum Schreiben der Properties eingebaut.

--- src/de/ewus/kv3/Kleber.java
+++ src/de/ewus/kv3/Kleber.java
@@ -1,6 +1,7 @@
 package de.ewus.kv3;
 import java.util.Properties;
 import ml.options.*;
+import java.io.*;
 
 /**
  * Die Klasse Kleber verbindet alle Hilfsklassen zur lauffähigen Anwendung.
@@ -12,19 +13,26 @@ import ml.options.*;
 public class Kleber {
     private UITypen targetui;
     //private Konfiguration konfig;
-    private Properties properties;
+    private Properties properties; // NOTE: Apache.org bietet unter http://jakarta.apache.org/commons/cli/index.html ebenfalls eine Bibliothek
     private UIManager ui;
     private Historie historie;
     private GfxEinstellungen gfxeinstellungen;
-
+    private ml.options.Options opt;
+    private boolean hasCLIOptions = false;
+    
+    private final String
+        CFGDATEI = "cfgDatei",
+        CFGFORMAT = "cfgFormat";
 
     /**
      * Constructor for objects of class Kleber
      *
      * @param  args  Die Übergabe der Kommandozeilenparameter
      */
-    public Kleber(String[] args) {
-        init(args);
+    public Kleber(String[] args) {         
+        opt = new Options(args, 0, 0);
+        hasCLIOptions = args.length > 0;
+        init();
         createUI();
         ui.startUI();
     }
@@ -54,6 +62,7 @@ public class Kleber {
     public void quitApp() {
         // QUESTION: System.exit oder ui.dispose();
         ui.dispose();
+        schreibProperties();
         //System.exit(0);
     }
 
@@ -74,31 +83,44 @@ public class Kleber {
     private void hinweiseKommandozeile(String msg) {
         System.out.println("Folgende Kommandozeilenparameter werden verarbeitet:");
         System.out.println(" -ui=[swing|console|ncurses]");
+        System.out.println(" -cfgFormat=[XML|TXT]");
+        System.out.println(" -cfgDatei=<datei.cfg>");
         if (!msg.equals("")) {
             System.out.println("\n" + msg);
         }
     }
-    
+
     /**
-     *  Hier werden Konfigurationsoptionen ausgewertet und gesetzt.
-     *
-     * @param  args  Die Kommandozeilenparameter
+     * Die Klasse opt wird befüllt.
      */
-    private void init(String[] args) {
-        ml.options.Options opt = new Options(args, 0, 1);
+    private void setzeCLIOptionen() {
+        // TODO: Verlinken von http://www.javaworld.com/javaworld/jw-08-2004/jw-0816-command.html und Lizenz abbilden        
         opt.getSet().addOption("ui", 
             ml.options.Options.Separator.EQUALS, 
             ml.options.Options.Multiplicity.ZERO_OR_ONE);
-        if (args.length > 0 && !opt.check(false,false)) {
+        opt.getSet().addOption("cfgFormat",
+            ml.options.Options.Separator.EQUALS, 
+            ml.options.Options.Multiplicity.ZERO_OR_ONE);
+        opt.getSet().addOption("cfgDatei",
+            ml.options.Options.Separator.EQUALS, 
+            ml.options.Options.Multiplicity.ZERO_OR_ONE);
+    }
+    
+    /**
+     *  Hier werden Konfigurationsoptionen ausgewertet und gesetzt.
+     */
+    private void init() {
+        setzeCLIOptionen();
+        if (hasCLIOptions && !opt.check(false,false)) {
             // Print usage hints
             hinweiseKommandozeile(opt.getCheckErrors());
             System.exit(1);
         } else {        
             historie = new Historie();
             //konfig = new Konfiguration("kv3konfig.txt", true);
-            // TODO: properties aus Datei laden
+            // DONE: properties aus Datei laden
             properties = new Properties();
-            
+            liesProperties();
             
             //Zuerst alle Optionen auf Standard setzen
             setzeUI("swing");
@@ -112,13 +134,66 @@ public class Kleber {
             
             //Setze Optionen aus der Kommandozeile
             // TODO: Setze Optionen aus der Kommandozeile
-            setzeUI(opt.getSet().getOption("ui").getResultValue(0));
+            if (opt.getSet().isSet("ui")) setzeUI(opt.getSet().getOption("ui").getResultValue(0));
     
         }
     }
 
+    
+    private void liesProperties() {
+        //In welchem Format legen wir die Konfiguration ab?
+        boolean useXMLcfg = true; //Standard ist XML
+        String cfgDatei = "", cfgFormat = "";
+        if (opt.getSet().isSet(CFGDATEI)) cfgDatei = opt.getSet().getOption(CFGDATEI).getResultValue(0);
+        
+        if (opt.getSet().isSet(CFGFORMAT)) {
+            cfgFormat = opt.getSet().getOption(CFGFORMAT).getResultValue(0);
+            if ( cfgFormat.equalsIgnoreCase("TXT") || cfgFormat.equalsIgnoreCase("XML"))
+                useXMLcfg = cfgFormat.equalsIgnoreCase("XML");
+            else
+                hinweiseKommandozeile("Parameter -" + CFGFORMAT + " hat einen ungültigen Wert (" + cfgFormat + ").");
+        }
+        //Schonmal abspeichern, welches Datenformat wir haben
+        properties.setProperty(CFGFORMAT, (useXMLcfg ? "XML" : "TXT"));
+        
+        //Und wie heißt die Konfigurationsdatei?
+        if (cfgDatei.equals("")) cfgDatei = "kv3.cfg";
+        File cfgFile = new File(cfgDatei);
+        try {
+            if (!cfgFile.canRead()) fehler("Datei \"" + cfgDatei + "\" ist nicht lesbar.");
+        } catch (SecurityException e) { fehler(e.getMessage()); }
+        //Und wir merken uns unserer Konfigurationsdatei
+        properties.setProperty(CFGDATEI, cfgDatei);
+    }
 
+    
+    private void schreibProperties() {
+        boolean useXMLcfg = true; //Standard ist XML
+        String cfgDatei = "", cfgFormat = "";
+        cfgDatei = properties.getProperty(CFGDATEI);
+        cfgFormat = properties.getProperty(CFGFORMAT);
+        File cfgFile = new File(cfgDatei);
+        try {
+            if (cfgFile.exists() && !cfgFile.canWrite()) fehler("Datei \"" + cfgDatei + "\" ist nicht schreibbar.");
+            if (!cfgFile.exists() && !cfgFile.createNewFile()) fehler("Datei \"" + cfgDatei + "\" kann nicht erstellt werden.");
+            FileOutputStream out = new FileOutputStream(cfgFile);
+            String comments = "KV3 - Konfigurationseinstellungen";
+            if (useXMLcfg) properties.storeToXML(out, comments);
+            else properties.store(out, comments);
+            out.close();
+            fehler("Datei gespeichert!");
+        }
+        catch (SecurityException e) { fehler(e.getMessage()); }
+        catch (IOException e) { fehler(e.getMessage()); }
+        catch (ClassCastException e) { fehler(e.getMessage()); }
+        catch (NullPointerException e) { fehler(e.getMessage()); }
+    }
 
+    private void fehler(String msg)
+    {
+        System.err.println(msg);
+    }
+    
     /**
      * Setzte die Oberfläche
      *
